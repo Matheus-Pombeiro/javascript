e41e5f08fb427a28743004de5e43ecfec314d868
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-04-28 18:11:07 -0300

    Atualizei o projeto Alura Midi

--- alura-midi/main.js
+++ alura-midi/main.js
@@ -1,11 +1,6 @@
+// Pom --------------------------------------------
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play(); 
 }
 
-document.querySelectorAll('.tecla')
-
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
-}
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap; llll
+const listaDeTeclas = document.querySelectorAll('.tecla');

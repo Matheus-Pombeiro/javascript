91d45a6d7c793a56e2d762ce0108a0978e13ac6c
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-04-30 15:54:34 -0300

    Atualizei o projeto Robotron 2000

---
+++ robotron-2000/js/main.js
@@ -0,0 +1,14 @@
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("cleventoick", (evento) => {
+    console.log(evento)
+});
+
+// Para declarar uma função anônima támbem é possível usar: () => {}
+
+function dizOi (nome) {
+    console.log(nome);
+    console.log("Bem-vindo ao Robotron 2000");
+}
+
+dizOi ("Matheus");

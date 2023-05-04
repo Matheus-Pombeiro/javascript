87947ccdc317b264a8724c075e4321c4144a8cfe
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-05-04 19:10:10 -0300

    Atualizei o desafio 4 do 7DaysOfCode

--- seven-days-of-code-alura-js/dia-4/main.js
+++ seven-days-of-code-alura-js/dia-4/main.js
@@ -3,12 +3,18 @@ let num = Math.floor(Math.random() * (7 - 1 + 1) + 1);
 
 // Processamento das respostas
 let tentativa;
+let acertou = false;
 for (tentativa = 0; tentativa < 3; tentativa++) {
     let advinha = prompt("Estou pensando em um número de 1 à 7... Tente advinhar!");
     if (advinha == num) {
        alert("Parabéns! Você acertou!");
-       tentativa = tentativa + 2; // Escrevi esta linha de código para parar o programa caso o jogador acerte na primeira ou segunda tentativa.
+       acertou = true;
+       break
     } else {
-        alert("Que pena... Você errou.");
+        alert("Você errou...");
     }
+}
+
+if (!acertou) {
+    alert(`Infelizmente você não acertou. O número era ${num}`);
 }

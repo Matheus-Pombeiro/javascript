083a6a66607bc6d0cd102816195a911491c63631
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-05-05 20:02:43 -0300

    Iniciei os meus estudos em conceitos básicos de JS com node.js

---
+++ conceitos-basicos-js/ex1.js
@@ -0,0 +1,16 @@
+// Estou aprendendo os conceitos básicos de JS por meio do node.js.
+console.log("Helo, World!");
+
+console.log("Trabalhando com variáveis.");
+
+const idade = 26;
+
+console.log("Idade");
+console.log(idade);
+console.log(idade + 2);
+console.log(idade - 2);
+console.log(idade / 2);
+
+const idadeSomada = idade + 2;
+
+console.log(idadeSomada);
---
+++ conceitos-basicos-js/ex2.js
@@ -0,0 +1,15 @@
+// JS é uma lingaugem case sensitive!
+
+// Console.log("Helo, World!"); --> retornaria um erro de referência, pois a primeira letra de "console..." foi escrita em caixa alta.
+
+console.log("Trabalhando com variáveis."); // Código funcional, pois está de acordo com a regra case sensitive.
+
+// Da mesma forma acontece com as variáveis. Veja:
+
+const idade = 20; // Variável escrita com letra minúscula.
+
+const Idade = 30; // Variável escrita com letra maiúscula. --> Tome muito cuidado com isso, pois pode não ser uma boa prática de programação!
+
+console.log(`idade = ${idade}`);
+
+console.log(`Idade = ${Idade}`);

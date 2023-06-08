213f46fd47ee2e070fff63a386552404d1ada6bf
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-06-08 19:33:38 -0300

    Ex. 1 do cap. 9 do livro de JS criado

---
+++ livrojs/cap9/ex9-1/main.js
@@ -0,0 +1,4 @@
+// Obtém os elementos da página
+const frm = document.querySelector("form");
+const imgClube = document.querySelector("#imgClube");
+const divTitulo = document.querySelector("#divTitulo");

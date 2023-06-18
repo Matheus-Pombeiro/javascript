cddad4795f8e32c21820028bb2c71e9cf0ccd215
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-06-18 10:14:19 -0300

    Ex. 4 do cap. 10 do livro de  JS atualizado

--- livrojs/cap10/ex10-4/main.js
+++ livrojs/cap10/ex10-4/main.js
@@ -2,50 +2,6 @@
 const frm = document.querySelector("form");
 const dvIdade = document.querySelector("#divIdade");
 
-// Declara a função mostrar velas para exibir as velas de aniversário para o usuário
-const mostrarVelas = (idade, centena, dezena, unidade) => {
-
-    if (idade >= 100) {     // Se a idade possui um nº na casa da centena...
-
-        // Cria tags 'img' no HTML da página
-        const velaC = document.createElement("img");;       
-        const velaA = document.createElement("img");
-        const velaB = document.createElement("img");
-
-        // Atribui uma 'img' para as variáveis da centena, dezena e unidade, respectivamente
-        velaC.src = "img/" + centena + ".jpg";                       
-        velaB.src = "img/" + dezena + ".jpg";               
-        velaA.src = "img/" + unidade + ".jpg";
-
-        // Define as variáveis das 'img' das velas como filhas de 'dvIdade'
-        dvIdade.appendChild(velaC);
-        dvIdade.appendChild(velaB);
-        dvIdade.appendChild(velaA);
-
-    } else if (idade >= 10) {       // Se a idade possui nº na casa decimal, mas não na casa da centena...
-
-        // Cria tags 'img' no HTML da página 
-        const velaB = document.createElement("img");
-        const velaA = document.createElement("img");
-
-        // Atribui uma 'img' para as variáveis da dezena e unidade, respectivamente
-        velaB.src = "img/" + dezena + ".jpg";
-        velaA.src = "img/" + unidade + ".jpg";
-
-        // Define as variáveis das 'img' das velas como filhas de 'dvIdade'
-        dvIdade.appendChild(velaB);
-        dvIdade.appendChild(velaA);
- 
-    } else {        // Senão...
-    
-      const velaA = document.createElement("img");      // Cria uma tag 'img' no HTML da página
-      velaA.src = "img/" + unidade + ".jpg";            // Atribui uma 'img' para as variáveis da unidade
-      dvIdade.appendChild(velaA);                       // Define a var da 'img' da vela como filha de 'dvIdade' 
-
-    }
-
-}
-
 // 'Escuta' o click no botão 'submit'
 frm.addEventListener("submit", (e) => {
 
@@ -53,12 +9,18 @@ frm.addEventListener("submit", (e) => {
 
     const idade = Number(frm.inIdade.value);        // Obtém a idade do usuário
 
-    // Obtém os números que compõe as casas da centena, dezena e unidade da idade inserida pelo usuário
-    const centena = Math.floor(idade / 100);
-    const dezena = Math.floor((idade / 10) % 10);
-    const unidade = idade % 10;
+    const strIdade = idade.toString();      // Converte a idade em string
 
-    mostrarVelas(idade, centena, dezena, unidade);     // Chama a função mostrar velas com passagem de parâmetros
+    for (const num of strIdade) {       // Percorre os caracteres da string
+        
+        const img = document.createElement("img");      // Cria uma imagem no HTML
+        img.src = `img/${num}.jpg`;                     // Atribui uma imagem ao elemento 'img'
+        img.alt = `Vela de ${num} anos`;                // Atribui um texto alternativo (acessibilidade) para o elemento 'img'
+        dvIdade.appendChild(img);                       // Define 'img' como filha de 'dvIdade'
+
+    }
+
+    frm.btExibir.disabled = true;       // Desabilita o botão 'btExibir'
 
 })
 
